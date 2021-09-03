23088795acc170e215deae49a2017d93dfdbbe32
INewTroNI <inew.troni@gmail.com>
2021-09-04 00:28:57 +0800

    Writen character class, enums for program, started work on data init for characters

--- a.cpp
+++ a.cpp
@@ -1,8 +1,69 @@
 #include "a.hpp"
 
-void main()
+bool Character::isOnTeam(bonusTeam t) const
 {
+	for(auto x : m_bonus_teams)
+	{
+		if (x == t)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void Character::print() const
+{
+	std::cout << "\n ID: " << static_cast<int>(m_id) << "\n Teams: ";
+	for(auto x : m_bonus_teams)
+	{
+		std::cout << static_cast<int>(x) << ", ";
+	}
+	std::cout << "\n";
+}
+
+auto getCharacterList()
+{
+	std::vector<Character> chList;
 	
+	using bt = bonusTeam;
+	using ch = character;
 	
-	return;
+	Character temp;
+	temp.setID(ch::BlackPanther).resetBonusTeams()
+	>> bt::TheCrew >> bt::MarvelRoyalty >> bt::AgileFighters
+	>> bt::Avengers >> bt::BackInBlack >> bt::CuttingEdge 
+	>> bt::BigBrains >> bt::FantasticFourReserveMembers;
+	chList.push_back(temp);
+	
+	temp.setID(ch::BlackWidow).resetBonusTeams()
+	>> bt::FemmeFatalis >> bt::AgentsOfSHIELD >> bt::WebWarriors
+	>> bt::AgileFighters >> bt::Avengers >> bt::WomenOfMarvel
+	>> bt::Partners >> bt::BackInBlack >> bt::SharpShooters;
+	chList.push_back(temp);
+	
+	temp.setID(ch::Blade).resetBonusTeams()
+	>> bt::Anti_Heroes >> bt::MidnightSons >> bt::GodsAndDeamons
+	>> bt::Avengers >> bt::BackInBlack >> bt::MartialArtists
+	>> bt::CuttingEdge >> bt::SharpShooters >> bt::MarvelKnights;
+	chList.push_back(temp);
+	
+	temp.setID(ch::Cable).resetBonusTeams()
+	>> bt::X_Men >> bt::X_Force >> bt::FamilyValues
+	>> bt::TeamLeaders >> bt::SharpShooters >> bt::HeavyMetal;
+	chList.push_back(temp);
+	
+	temp.setID(ch::Colossus).resetBonusTeams()
+	>> bt::X_Men >> bt::HeavyHitters >> bt::All_NewX_Men
+	>> bt::HeavyMetal;
+	chList.push_back(temp);
+	
+	return 0;
+}
+
+int main()
+{
+	getCharacterList();
+	
+	return 0;
 }
--- a.hpp
+++ a.hpp
@@ -1,14 +1,11 @@
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
-enum class teamUpBonus : std::uint_fast8_t
+//
+enum class bonusTeam : std::uint_fast8_t
 {
-	bonus1_2_4,
-	bonus2_4_6,
-	bonus2_4_7,
-	bonus2_4_8,
-	bonus3_6_10,
-	
+	none,
 	NewAvengers,
 	GuardiansOfTheGalaxy,
 	FemmeFatalis,
@@ -47,4 +44,147 @@ enum class teamUpBonus : std::uint_fast8_t
 	FantasticFour,
 	FantasticFourReserveMembers,
 	SmoothOperators
+};
+
+enum class bonusPower : std::uint_fast8_t
+{
+	none,
+	bonus1_2_4,
+	bonus2_4_6,
+	bonus2_4_7,
+	bonus2_4_8,
+	bonus3_6_10,
+};
+
+enum class bonusType : std::uint_fast8_t
+{
+	none,
+	str,
+	dur,
+	mas,
+	res,
+	vit,
+	ene
+};
+
+enum class character : std::uint_fast8_t
+{
+	none,
+	BlackPanther,
+	BlackWidow,
+	Blade,
+	Cable,
+	Colossus,
+	CptAmerica,
+	CptMarvel,
+	Crystal,
+	Cyclops,
+	Daredevil,
+	Deadpool,
+	DrDoom,
+	DrStrange,
+	Drax,
+	Elektra,
+	ElsaBloodstone,
+	Falcon,
+	Gambit,
+	Gamora,
+	GhostRider,
+	Hawkeye,
+	Hulk,
+	HumanTorch,
+	Iceman,
+	InvisibleWoman,
+	IronFist,
+	IronMan,
+	Loki,
+	LukeCage,
+	Magneto,
+	MilesMorales,
+	MoonKnight,
+	Morbius,
+	MrFantastic,
+	MsMarvel,
+	Nightcrawler,
+	Phoenix,
+	Psylocke,
+	Punisher,
+	RocketAndGroot,
+	ScarletWitch,
+	Spider_Gwen,
+	Spider_Man,
+	Star_Lord,
+	Storm,
+	Thanos,
+	TheThing,
+	Thor,
+	Venom,
+	Wasp,
+	Wolverine
+};
+
+class TeamBonus
+{
+private:
+	const bonusTeam m_id{ bonusTeam::none };
+	const bonusPower m_power{ bonusPower::none };
+	const bonusType m_type{ bonusType::none };
+	
+public:
+	TeamBonus(bonusTeam id, bonusPower power, bonusType type) : m_id{ id }, m_power{ power }, m_type{ type }
+	{ }
+	bonusTeam getID() const { return m_id; }
+	bonusPower getPower() const { return m_power; }
+	bonusType getType() const { return m_type; }
+	
+	// operators for comparison
+};
+
+class Character
+{
+private:
+	character m_id{ character::none };
+	std::vector<bonusTeam> m_bonus_teams;
+	
+public:
+	Character(character id = character::none) : m_id{ id }
+	{ }
+	
+	character getID() const { return m_id; }
+	
+	const auto& getBonusTeams() const { return m_bonus_teams; }
+	
+	bool isOnTeam(bonusTeam t) const;
+	
+	Character& setID(const character id)
+	{
+		m_id = id;
+		return *this;
+	}
+	
+	Character& addToBonusTeam(const bonusTeam id)
+	{
+		m_bonus_teams.push_back(id);
+		return *this;
+	}
+	
+	Character& resetBonusTeams()
+	{
+		m_bonus_teams.clear();
+		return *this;
+	}
+	
+	void print() const;
+	
+	friend Character& operator>>(Character& ch, const bonusTeam t)
+	{
+		ch.addToBonusTeam(t);
+		return ch;
+	}
+	// operators for comparison?
+};
+
+class Team
+{
+	
 };
